0ea9f4f2b0ee8172edb09f532a0d93ed249427cd
Nur Sasongko <32429721+ssasongko@users.noreply.github.com>
2020-10-15 21:43:20 +0700

    Add files via upload

---
+++ PBOIF2-10119049-Latihan9-Teks/src/pboif2/pkg10119049/latihan9/teks/PBOIF210119049Latihan9Teks.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119049.latihan9.teks;
+
+/**
+ * @author
+ * Nama  : Nur Sasongko
+ * Kelas : PBOIF2
+ * NIM   : 10119049
+ * Deskripsi Program : Program menampilkan teks.
+ */
+public class PBOIF210119049Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+}
